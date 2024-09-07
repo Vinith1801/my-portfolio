f9885c710624592b0e87a580e0b4cf26e459a6b3
Vinith Bylapudi <Vinith1801>
2024-09-07 19:04:31 +0530

    added portfolio and contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,22 @@ menuIcon.addEventListener('click', () => {
         menuIcon.innerHTML = '<span class="icon">&#9776;</span>';
     }
 });
+
+function prevSlide(button) {
+    const carouselInner = button.parentElement.querySelector('.carousel-inner');
+    const firstChild = carouselInner.firstElementChild;
+    carouselInner.appendChild(firstChild);
+    updateCarouselPosition(carouselInner);
+}
+
+function nextSlide(button) {
+    const carouselInner = button.parentElement.querySelector('.carousel-inner');
+    const lastChild = carouselInner.lastElementChild;
+    carouselInner.insertBefore(lastChild, carouselInner.firstElementChild);
+    updateCarouselPosition(carouselInner);
+}
+
+function updateCarouselPosition(carouselInner) {
+    const width = carouselInner.clientWidth;
+    carouselInner.style.transform = `translateX(-${width}px)`;
+}
